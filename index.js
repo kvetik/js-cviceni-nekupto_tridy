4e895d94810710ccdb3af814e2434c03e7d7c93b
Kveta Kazova <kveti@Kveti.local>
2025-03-23 19:19:41 -0600

    Final result

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log('JavaScript se spustil!');
+
 const product1 = {
   image: 'images/destniklobouk.jpg',
   name: 'Deštníklobouk',
@@ -19,8 +21,36 @@ const product3 = {
 const firstCard = document.querySelector('#product1');
 firstCard.classList.add('border-primary'); // pridano oznaceni border pro prvni produkt
 
-const secondCardButton = document.querySelector('#product2 button');
-secondCardButton.classList.remove('btn-primary');
+const product1HTML = `
+  <img class="card-img-top" src="${product1.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
 
-const thirdCardTitle = document.querySelector('#product3 .card-title');
-thirdCardTitle.classList.toggle('text-center');
+const product1Elm = document.querySelector('#product1');
+product1Elm.innerHTML = product1HTML;
+
+const product2HTML = `
+  <img class="card-img-top" src="${product2.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product2.name}</h2>
+    <p class="card-text">${product2.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+const product2Elm = document.querySelector('#product2');
+product2Elm.innerHTML = product2HTML;
+
+const product3HTML = `
+  <img class="card-img-top" src="${product3.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product3.name}</h2>
+    <p class="card-text">${product3.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+const product3Elm = document.querySelector('#product3');
+product3Elm.innerHTML = product3HTML;
